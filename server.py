c5c5196a81b0bdab3c068db6b7cd514916ee2cb2
arek544 <arek544@gmail.com>
2022-08-17 16:25:20 +0200

    🎨 code aesthetic improvements

--- server.py
+++ server.py
@@ -4,10 +4,12 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # responding to get request
-@app.route('/', methods=['Get'])
+@app.route('/', methods=['GET'])
 def index():
-    return json.dumps({'name': 'Peter',
-                       'surname': 'Parker'})
+    return json.dumps({
+        'name': 'Peter',
+        'surname': 'Parker'
+    })
 
 # responding to post request
 @app.route('/', methods=['POST'])
